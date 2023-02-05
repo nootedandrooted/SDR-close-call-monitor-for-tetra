9aee6e7fc6646b47dce4e8255e30c0d4bd6d8fd2
nootedandrooted <114921570+nootedandrooted@users.noreply.github.com>
2023-02-05 20:49:59 +0200

    Update script.py

--- script.py
+++ script.py
@@ -1,20 +1,26 @@
 import os
 import numpy as np
+import time
 from rtlsdr import RtlSdr
+from pylab import *
+from rtlsdr import *
 
 # Set the SDR device parameters
 sdr = RtlSdr()
 sdr.sample_rate = 2.4e6  # Hz
 sdr.center_freq = 385e6  # Hz
 sdr.freq_correction = 60   # PPM
-sdr.gain = 'auto'
+sdr.gain = 40
 
-# Set the frequency range to scan
+# Set the frequency range to scan (380MHz-400MHz)
 start_freq = 380e6  # Hz
-end_freq = 390e6  # Hz
+end_freq = 400e6  # Hz
 
 # Set the number of samples to read at a time
 num_samples = 2**16
+
+# Set the threshold for the squelch level
+squelch_level = 100
 
 while True:
     # Read samples from the SDR device
@@ -23,5 +29,14 @@ while True:
     spectrum = np.abs(np.fft.fftshift(np.fft.fft(samples)))
     # Find the peak frequency in the spectrum
     peak_freq = np.argmax(spectrum)
-    # Print the peak frequency to the command line
-    print("Peak frequency: {:.6f} MHz".format(peak_freq / 1e6 + start_freq / 1e6))
+    # Only print the peak frequency if it exceeds the squelch level and is not excluded
+    if spectrum[peak_freq] > squelch_level:
+        print("Peak frequency: {:.6f} MHz".format(peak_freq / 1e6 + start_freq / 1e6))
+
+        #Plot the PSD with matplotlib
+        psd(samples, NFFT=1024, Fs=sdr.sample_rate/1e6, Fc=sdr.center_freq/1e6)
+        xlabel('Frequency (MHz)')
+        ylabel('Relative power (dB)')
+        show()
+        
+sdr.close()
