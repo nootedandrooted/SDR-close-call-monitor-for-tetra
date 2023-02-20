da2e054803ea3a2abadeb6a3fa7f7cb40e573a79
nootedandrooted <114921570+nootedandrooted@users.noreply.github.com>
2023-02-20 16:40:56 +0200

    Update script.py

--- script.py
+++ script.py
@@ -1,3 +1,4 @@
+import time
 import numpy as np
 from rtlsdr import RtlSdr
 from playsound import playsound
@@ -10,15 +11,53 @@ sdr.freq_correction = 60   # PPM
 sdr.gain = 30              # dB
 
 # Set the frequency range to scan (390MHz-392.5MHz)
-start_freq = 433e6  # Hz
-end_freq = 435e6  # Hz
-sdr.set_center_freq((start_freq + end_freq) / 2)
+start_freq = 392e6  # Hz
+end_freq = 394e6  # Hz
+sdr.center_freq = (start_freq + end_freq) / 2
+
 
 # Set the number of samples to read at a time
 num_samples = 2**16
 
 # Set the threshold for the squelch level
 squelch_level = -40
+
+# Create the blacklist
+blacklist = []
+
+
+# Run for 60 seconds
+print("Scanning for frequencies to be blacklisted. This will take a minute.")
+t_end = time.time() + 60
+while time.time() < t_end:
+
+    # Read samples from the SDR device
+    samples3 = sdr.read_samples(num_samples)
+
+    # Calculate the frequency range of the samples
+    freq_range3 = np.linspace(start_freq, end_freq, num_samples, endpoint=False)
+    
+    # Convert the samples to a power spectrum
+    spectrum3 = np.abs(np.fft.fftshift(np.fft.fft(samples3)))
+    
+    # Find the peak frequency in the spectrum
+    peak_index3 = np.argmax(spectrum3)
+    peak_freq3 = freq_range3[peak_index3]
+
+    # Convert peak frequency to MHz
+    peak_freq_mhz3 = f"{peak_freq3 / 1e6:.9f}"
+
+    for i in blacklist:
+        if i == peak_freq_mhz3:
+            break
+    else:
+        blacklist.append(peak_freq_mhz3)
+        print("Added frequency to blacklist")
+
+print(f"The following frequencie(s) are blacklisted: {blacklist}")
+print("Blacklist created successfully!")
+
+
 
 while True:
     # Read samples from the SDR device
@@ -51,35 +90,13 @@ while True:
     peak_freq2 = freq_range2[peak_index2]
 
     # Convert peak frequency to MHz
-    peak_freq2_mhz = f"{peak_freq2 / 1e6:.9f}"
+    peak_freq2_mhz2 = f"{peak_freq2 / 1e6:.9f}"
 
     # Only print the peak frequency if it exceeds the squelch level and is not excluded
-    # The frequencies are excluded using a filter size 34.2 KHz
-    if spectrum[peak_index] > squelch_level:
-        if (peak_freq > 393489966) and (peak_freq < 393490034):
-            pass
-        elif (peak_freq > 393339966) and (peak_freq < 393340034):
-            pass
-        elif (peak_freq > 393239966) and (peak_freq < 393240034):
-            pass
-        elif (peak_freq > 393089966) and (peak_freq < 393090034):
-            pass
-        elif (peak_freq > 393064966) and (peak_freq < 393065034):
-            pass
-        elif (peak_freq > 393029966) and (peak_freq < 393030034):
-            pass
-        elif (peak_freq > 393389966) and (peak_freq < 393390034):
-            pass
-        elif (peak_freq > 391159966) and (peak_freq < 391160034):
-            pass
-        elif (peak_freq > 391089966) and (peak_freq < 391090034):
-            pass
-        elif (peak_freq > 390864966) and (peak_freq < 390865034):
-            pass
-        elif (peak_freq > 390794966) and (peak_freq < 390795034):
-            pass
-        else:
-            print(f"Peak frequency: {peak_freq_mhz} MHz")
-            if abs(peak_freq2 - peak_freq) > 2000:
-                print(peak_freq_mhz)
-                playsound('/home/pop/Downloads/warning.wav')
+    if spectrum[peak_index] > squelch_level and peak_freq_mhz in blacklist:
+        pass
+    else:
+        print(f"Peak frequency detected but no enough difference between samples...: {peak_freq_mhz} MHz")
+        if abs(peak_freq2 - peak_freq) > 10000:
+            print(f"Peak frequency detected: {peak_freq_mhz}Mhz")
+            playsound('/home/pop/Downloads/warning.wav')
